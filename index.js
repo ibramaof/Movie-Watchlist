f14c7e9dd183019c5cbe21527fb7378c119f1059
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2026-04-19 21:19:56 +0200

    finished search results design and displaying results using js (API)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const searchBtn = document.getElementById('search-bar')
+const searchBtn = document.getElementById('search-btn')
 const searchText = document.getElementById('search-text')
 const mainLayOut = document.getElementById('main-layout')
+const noData = document.getElementById('no-data')
+const noFilmFound = document.getElementById('no-film')
 const apiKey = '338ca256'
-const movieName = searchText.value
 
 
 
@@ -14,23 +15,65 @@ const movieName = searchText.value
 searchBtn.addEventListener('click', fetchForFilms)
 
 async function fetchForFilms() {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}`)
+    const movieName = searchText.value
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${movieName}&t=${movieName}`)
     const data = await response.json()
-    const filmsIdsArr = data.Search.map(film => film.imdbID)
-    fetchForFilmDetails(filmsIdsArr)
+    if (data.Search) {
+        const filmsIdsArr = data.Search.map(film => film.imdbID)
+        console.log(data)
+        fetchForFilmDetails(filmsIdsArr)
+    } else {
+        noFilmFound.textContent = 'Unable to find what you’re looking for. Please try another search.'
+    }
+
 }
 
 function renderCard(data) {
+    let results = ''
+    for (let film of data) {
 
+        results += `
+    <div class="movie-card" id="moive-card">
+            <div class="poster-div" id="poster-div">
+                <img src=${film.Poster}
+                    class="poster" id="poster">
+            </div>
+            <div class="movie-info" id="movie-info">
+                <div class="header-title" id="header-title">
+                    <p class="movie-title" id="movie-title">${film.Title}</p>
+                    <div class="movie-rating" id="movie-raring">
+                        <span class="material-symbols-outlined">
+                            star
+                        </span>
+                        <p class="rating" id="rating">${film.imdbRating}</p>
+                    </div>
+                </div>
+                <div class="movie-data" id="movie-data">
+                    <p class="runtime" id="runtime"> ${film.Runtime}</p>
+                    <p class="genre" id="genre">${film.Genre}</p>
+                    <button class="add-btn" id="add-bar"><i class="fa-solid fa-circle-plus fa-shake"></i></i>
+                        Watchlist</button>
+                </div>
+                <p class="plot" id="plot">
+                    ${film.Plot}
+                </p>
+            </div>
+    </div>
+    <div class="divider" id="divider"></div> 
+`
+
+    }
+    noData.style.display = 'none'
+    document.getElementById('results').innerHTML = results
 }
 
 
 async function fetchForFilmDetails(filmsIdsArr) {
 
-    const films = filmsIdsArr.map(async id => {
+    const films = await Promise.all(filmsIdsArr.map(async id => {
         const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
         const data = await response.json()
         return data
-    })
-    console.log(films)
+    }))
+    renderCard(films)
 }
